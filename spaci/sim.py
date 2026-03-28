94b772fff81c7d5ccfa73a0a128fe5b6ba1204ad
hoangnm97 <hoangnm97s@gmail.com>
2026-03-28 16:02:52 +0700

    spacy vec and sim

---
+++ spaci/sim.py
@@ -0,0 +1,19 @@
+import spacy
+
+nlp = spacy.load("en_core_web_lg")
+# word vectors
+tokens = nlp("dog cat banana afskfsd")
+
+for token in tokens:
+    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
+
+# similarity
+doc1 = nlp("I like salty fries and hamburgers.")
+doc2 = nlp("Fast food tastes very good.")
+
+# -- Similarity of two documents
+print(doc1, "<->", doc2, doc1.similarity(doc2))
+# -- Similarity of tokens and spans
+french_fries = doc1[2:4]
+burgers = doc1[5]
+print(french_fries, "<->", burgers, french_fries.similarity(burgers))
