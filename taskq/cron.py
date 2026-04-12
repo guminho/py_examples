db57321b7865494cafb45309fc897c7ce0423351
hoangnm97 <hoangnm97s@gmail.com>
2026-04-12 18:08:30 +0700

    update taskiq scheduler

--- taskq/cron.py
+++ taskq/cron.py
@@ -1,19 +1,32 @@
+import asyncio
+from datetime import timedelta
+
 from taskiq import TaskiqScheduler
 from taskiq.schedule_sources import LabelScheduleSource
+from taskiq.scheduler.scheduled_task import ScheduledTask
 from taskiq_redis import RedisStreamBroker
 
 broker = RedisStreamBroker(
     url="redis://localhost:6379/0",
     queue_name="taskq1",
 )
-
-cron_src = LabelScheduleSource(broker)
+schedule_source = LabelScheduleSource(
+    broker=broker,
+)
 scheduler = TaskiqScheduler(
     broker=broker,
-    sources=[cron_src],
+    sources=[schedule_source],
 )
 
+schedule_heavy_task: ScheduledTask = {
+    "interval": timedelta(seconds=5),
+    # "cron": "* * * * *",
+    "args": [5],
+    "labels": {"mykey": "math"},
+}
 
-@broker.task(schedule=[{"cron": "*/1 * * * *", "args": [1]}])
-async def heavy_task(value: int) -> int:
-    return value + 1
+
+@broker.task(schedule=[schedule_heavy_task])
+async def heavy_task(x1: int, x2: int = 2) -> int:
+    await asyncio.sleep(0.05)
+    print(x1 + x2)
